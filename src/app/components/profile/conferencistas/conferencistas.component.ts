cf13b65599c681a03e1873a9a6f9205cf46144fa
Fabian Zapata <fabiing10@gmail.com>
2020-08-07 01:20:47 -0500

    Bloques setup

--- src/app/components/profile/conferencistas/conferencistas.component.ts
+++ src/app/components/profile/conferencistas/conferencistas.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-conferencistas',
@@ -6,10 +8,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./conferencistas.component.css']
 })
 export class ConferencistasComponent implements OnInit {
-
-  constructor() { }
+  @Input() idioma: any;
+  public live_video;
+  tabTools: string = 'bloque_01';
+  constructor(
+    private _translate: TranslateService,
+    private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit() {
+    this.setVideo('bloque_01')
   }
 
+  ngOnChanges(){
+    console.log('algo cambio')
+    console.log(this.idioma)
+    console.log(this.tabTools)
+    this.setVideo(this.tabTools)
+  }
+
+  setVideo(bloque?){
+
+    let lang = localStorage.getItem('lang')
+    let bloque_string = bloque.toString();
+    let lang_string = lang.toString();
+    let video = bloque_string+"."+lang_string;
+
+    this._translate.setDefaultLang(lang);
+    this._translate.get(video).subscribe((res) => {
+    this.live_video = this.sanitizer.bypassSecurityTrustResourceUrl(res); 
+    
+    });
+  }
+
+  changeTabTool(event) {
+    this.tabTools = event;
+    this.setVideo(event);
+  }
+  
 }
